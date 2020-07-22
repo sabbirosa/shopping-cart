82af961a1bea2c5d1a4fdda0461d20519ad5f029
sabbirosa <sabbir0727@gmail.com>
2020-07-22 23:01:08 +0600

    Just getting started!

---
+++ main.js
@@ -0,0 +1,4 @@
+const addProductBtn = document.getElementById("addProductBtn");
+addProductBtn.addEventListener("click", function() {
+    const currentProductAmount = document.getElementById("")
+})
