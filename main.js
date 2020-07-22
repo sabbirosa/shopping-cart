1b818804dbd7f2386ed19640a94deac3080df01a
sabbirosa <sabbir0727@gmail.com>
2020-07-22 23:20:51 +0600

    Plus button working successfully!

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const addProductBtn = document.getElementById("addProductBtn");
-addProductBtn.addEventListener("click", function() {
-    const currentProductAmount = document.getElementById("")
+const addProductBtn1 = document.getElementById("addProductBtn1");
+addProductBtn1.addEventListener("click", function() {
+    const currentProductAmount = document.getElementById("productCount1").value;
+    const currentProductAmountNumber = parseFloat(currentProductAmount);
+    const newProductAmount = currentProductAmountNumber + 1;
+   
+    document.getElementById("productCount1").value = newProductAmount;
 })
