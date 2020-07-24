070592d7f4708699c7c2bc0718748a9329d9af7f
sabbirosa <sabbir0727@gmail.com>
2020-07-24 22:32:42 +0600

    Changing full function!

--- main.js
+++
@@ -1,8 +0,0 @@
-const addProductBtn1 = document.getElementById("addProductBtn1");
-addProductBtn1.addEventListener("click", function() {
-    const currentProductAmount = document.getElementById("productCount1").value;
-    const currentProductAmountNumber = parseFloat(currentProductAmount);
-    const newProductAmount = currentProductAmountNumber + 1;
-   
-    document.getElementById("productCount1").value = newProductAmount;
-})
